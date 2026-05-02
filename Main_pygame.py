4370e962c8e1b911e779d6e7bbd37055584805c3
unknown <erickvaleriochaves@gmail.com>
2026-05-02 12:15:21 -0300

    Refatorando a logica do movimento

--- Main_pygame.py
+++ Main_pygame.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+from snake_logic import SnakeGame
 
 assets  = {
     'head_up' : pygame.image.load('Graphics/head_up.png'),
@@ -18,6 +20,40 @@ assets  = {
     'apple' : pygame.image.load('Graphics/apple.png')
 }
 
+def get_segment_image(game, index, assets):
+    current = game.snake[index]
+    
+    #cabeça
+    if index == 0:
+        return assets[f'head_{game.current_direction}']
+    
+    #cauda
+    if index == len(game.snake) - 1:
+        previous = game.snake[index -1]
+        dx, dy = previous[0] - current[0], previous[1] - current[1]
+        
+        if dx > 1: dx= -1
+        elif dx < -1: dx = 1
+        if dy > 1: dy = -1
+        elif dy < -1: dy = 1
+        dir = 'up' if dy == -1 else 'down' if dy == 1 else 'left' if dx == -1 else 'right'
+        return assets[f'tail_{dir}']
+    
+    #corpo
+    previous, next = game.snake[index-1], game.snake[index + 1]
+    p_dx, p_dy = previous[0] - current[0], previous[1] - current[1]
+    n_dx, n_dy = next[0] - current[0], next[1] - current[1]
+    
+    if abs(p_dx) > 1: p_dx = -1 if p_dx > 0 else 1
+    if abs(n_dx) > 1: n_dx = -1 if n_dx > 0 else 1
+    if abs(p_dy) > 1: p_dy = -1 if p_dy > 0 else 1
+    if abs(n_dy) > 1: n_dy = -1 if n_dy > 0 else 1
+    
+    if p_dx == n_dx: return assets['body_vertical']
+    if p_dy == n_dy: return assets['body_horizontal']
+    
+    return assets['body_horizontal']
+    
 def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 640))
--- snake_logic.py
+++ snake_logic.py
@@ -2,24 +2,34 @@ import random
 
 class SnakeGame:
     def __init__(self, width, height):
-            self.width = width
-            self.height = height
-            self.snake = [(0,0)]
-            self.fruits = []
-            self.is_alive = True
-            self.spawn_fruits()
+        self.width = width
+        self.height = height
+        self.snake = [(5, 5), (4, 5), (3, 5)] 
+        self.fruits = []
+        self.is_alive = True
+        self.current_direction = 'd'
+        self.spawn_fruits()
             
     def move(self, direction):
+        if not self.is_alive: return
+        
+        
+        opposites = {'w': 's', 's': 'w', 'a': 'd', 'd': 'a'}
+        if direction in ['w', 's', 'a', 'd'] and direction != opposites.get(self.current_direction):
+            self.current_direction = direction
+
         x, y = self.snake[0]
-        dirs = {'w': (0,-1), 's':(0,1), 'a': (-1,0), 'd': (1,0)}
-        dx, dy = dirs[direction]
-        new_head = ((x + dx) % self.width, (y+dy) % self.height)
+        dx, dy = {'w': (0,-1), 's':(0,1), 'a': (-1,0), 'd': (1,0)}[self.current_direction]
+        
+        
+        new_head = ((x + dx) % self.width, (y + dy) % self.height)
+        
         if new_head in self.snake:
             self.is_alive = False
             return
         
+        self.snake.insert(0, new_head)
         
-        self.snake.insert(0,new_head)
         if new_head in self.fruits:
             self.fruits.remove(new_head)
             self.spawn_fruits()
@@ -27,8 +37,8 @@ class SnakeGame:
             self.snake.pop()
         
     def spawn_fruits(self):
-        max = (len(self.snake) // 10) + 1
-        while len (self.fruits) < max:
+        max_fruits = (len(self.snake) // 10) + 1
+        while len(self.fruits) < max_fruits:
             f = (random.randint(0, self.width-1), random.randint(0, self.height-1))
             if f not in self.snake and f not in self.fruits:
                 self.fruits.append(f)
